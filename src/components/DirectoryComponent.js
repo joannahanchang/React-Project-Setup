6ccb75385ec50bf561694fb8a7e6ea923fa9d211
JoannaChang <joannahanchang@gmail.com>
2020-08-09 21:58:43 -0700

    Header and Footer

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import React from 'react';
 // const Component = React.Component
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-class Directory extends Component {
+function Directory (props) {
 
-    // renderSelectedCampsite(campsite) {
-    //     if (campsite) {
-    //         return (
-    //             <Card>
-    //                 <CardImg top src={campsite.image} alt={campsite.name} />
-    //                 <CardBody>
-    //                     <CardTitle>{campsite.name}</CardTitle>
-    //                     <CardText>{campsite.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
-    //         );
-    //     }
-    //     return <div />;
-    // }
-
-    render() {
-        const directory = this.props.campsites.map(campsite => {
+        const directory = props.campsites.map(campsite => {
             return (
                 <div key={campsite.id}className="col-md-5 m-1">
-                    <Card onClick={()=> this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
                 </div>
             );
         });
@@ -43,7 +32,6 @@ class Directory extends Component {
             </div>
         );
     }
-}
 
 // class ExampleParentComponent extends Component {
 //     constructor(props) {
