562bf8e48f0cc93829c51ae0e16a2017d542c751
JoannaChang <joannahanchang@gmail.com>
2020-08-26 22:40:53 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -50,7 +50,7 @@ class CommentForm extends Component {
         // console.log('this', this)
         // console.log('values', values)
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         // console.log('Current State is: ' + JASON.stringify(values));
         // // alert(`Rating: ${values.rating} Author: ${values.author} Text: ${values.text}`);
         // alert('Current State is: ' + JASON.stringify(values));
@@ -169,7 +169,7 @@ function RenderCampsite({campsite}) {
     return <div />;
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -182,7 +182,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+            <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -236,7 +236,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
